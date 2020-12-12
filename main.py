c3a0c2194fe135fdf977b9917a982af41a2ca6b4
avmiloserdov <miloserdoff.alex@yandex.ru>
2020-12-12 17:28:46 +0300

    added new normal div for comparison

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import statistics as st
 from matplotlib import pylab
 import scipy.stats as stats
-from scipy.special import kolmogorov
 
 
 def make_arr():
     data = []
-    with open("K_v30c.txt") as file:
+    with open("K_v30b.txt") as file:
         for line in file:
             data = np.append(data, [float(x.replace(',', '.')) for x in line.split()])
     return data
 
 
 dt = sorted(make_arr())
-print(dt)
-print(stats.kstest(dt, 'norm'))
+kol = len(dt)
+avg = st.mean(dt)
+np.random.seed(2)
+x = np.random.normal(loc=avg, scale=0.02, size=kol)
+# print(x)
 
-plt.hist(dt, 30, density=True)
+print(stats.ks_2samp(dt, x))
+# print(len(dt))
+
+# stats.probplot(dt, dist="norm", plot=pylab)
+# pylab.show()
+
+plt.hist(x, 20, density=True)
 plt.xlabel('Величина')
-plt.ylabel('Кол-во показаний')
-plt.title('Гистограмма измерений')
+plt.ylabel('Кол-во измерений')
+plt.title('Нормальное распределение')
 plt.grid()
 plt.show()
 #
-# stats.probplot(make_arr(), dist="norm", plot=pylab)
-# pylab.show()
-
-
-# def ecdf(dt):
-#     """ Compute ECDF """
-#     x = np.sort(dt)
-#     n = x.size
-#     y = np.arange(1, n+1) / n
-#     return(x,y)
-
-
-# x, y = ecdf(dt)
-# # mu, sigma = 5, 1
-# # rand_normal = np.random.normal(mu, sigma, 100)
-# # x, y = ecdf(rand_normal)
-# plt.scatter(x=x, y=y)
-# plt.show()
+plt.hist(dt, 20, density=True)
+plt.xlabel('Величина')
+plt.ylabel('Кол-во измерений')
+plt.title('Гистограмма эмперических данных')
+plt.grid()
+plt.show()
