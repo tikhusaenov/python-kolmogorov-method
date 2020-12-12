5733ee3f5a1ddd5e43c288f3894d82b5d6d26fc7
Tim <50641103+tikhusaenov@users.noreply.github.com>
2020-12-12 17:29:16 +0300

    df

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import pylab
 import scipy.stats as stats
+from scipy.stats import norm
 
 
 def make_arr():
     data = []
-    with open("K_v30a.txt") as file:
+    with open("K_v30c.txt") as file:
         for line in file:
             data = np.append(data, [float(x.replace(',', '.')) for x in line.split()])
     return data
 
 
-print(stats.kstest(make_arr(), 'norm'))
-plt.hist(make_arr())
-plt.show()
 
-stats.probplot(make_arr(), dist="norm", plot=pylab)
-pylab.show()
+
+data = make_arr()
+def ecdf():
+    x = np.sort(data)
+    n = x.size
+    y = np.arange(1, n+1) / n
+    return x, y
+
+
+print(ecdf())
+
+
+
+# print(stats.kstest(data_array, 'norm',))
+# stats.probplot(data_array, dist="norm", plot=pylab)
+# pylab.show()
+
 
